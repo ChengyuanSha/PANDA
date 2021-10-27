5115a284ade30fbc8d21685bd485e01ecb36f742
ChengyuanSha <15cs69@queensu.ca>
2021-10-27 17:26:09 -0400

    code optimization

--- src/general_GNN.py
+++ src/general_GNN.py
@@ -8,39 +8,27 @@ import torch_geometric.nn as pyg_nn
 
 class GNNStack(nn.Module):
     ''' The GNN Model '''
-    def __init__(self, input_dim, hidden_dim, output_dim, task='node'):
+    def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim):
         super(GNNStack, self).__init__()
-        self.task = task
+        # graph convolution layers
         self.convs = nn.ModuleList()
-        self.convs.append(self.__build_conv_model(input_dim, hidden_dim))
+        self.convs.append(pyg_nn.GCNConv(input_dim, hidden_dim1))
+        self.convs.append(pyg_nn.GCNConv(hidden_dim1, hidden_dim2))
+        self.convs.append(pyg_nn.GCNConv(hidden_dim2, hidden_dim2))
+        # layer norm
         self.lns = nn.ModuleList()
-        self.lns.append(nn.LayerNorm(hidden_dim))
-        self.lns.append(nn.LayerNorm(hidden_dim))
-        for l in range(2):
-            self.convs.append(self.__build_conv_model(hidden_dim, hidden_dim))
-
+        self.lns.append(nn.LayerNorm(hidden_dim1))
+        self.lns.append(nn.LayerNorm(hidden_dim2))
         # post-message-passing
         self.post_mp = nn.Sequential(
-            nn.Linear(hidden_dim, hidden_dim), nn.Dropout(0.25),
-            nn.Linear(hidden_dim, output_dim))
-        if not (self.task == 'node' or self.task == 'graph'):
-            raise RuntimeError('Unknown task.')
+            nn.Linear(hidden_dim2, hidden_dim2), nn.Dropout(0.25),
+            nn.Linear(hidden_dim2, output_dim))
 
         self.dropout = 0.25
         self.num_layers = 3
 
-    def __build_conv_model(self, input_dim, hidden_dim):
-        # refer to pytorch geometric nn module for different implementation of GNNs.
-        if self.task == 'node':
-            return pyg_nn.GCNConv(input_dim, hidden_dim)
-        else:
-            return pyg_nn.GINConv(nn.Sequential(nn.Linear(input_dim, hidden_dim),
-                                  nn.ReLU(), nn.Linear(hidden_dim, hidden_dim)))
 
-    def forward(self, x, edge_index, batch):
-        # if data.num_node_features == 0:
-        #   x = torch.ones(data.num_nodes, 1)
-
+    def forward(self, x, edge_index):
         for i in range(self.num_layers):
             x = self.convs[i](x, edge_index)
             emb = x
@@ -49,12 +37,10 @@ class GNNStack(nn.Module):
             if not i == self.num_layers - 1: # except last layer
                 x = self.lns[i](x)
 
-        if self.task == 'graph':
-            x = pyg_nn.global_mean_pool(x, batch)
-
         x = self.post_mp(x)
 
         return emb, F.log_softmax(x, dim=1)
 
     def loss(self, pred, label):
         return F.nll_loss(pred, label)
+
