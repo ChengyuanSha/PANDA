828de79a6f083a5fa05491981913c98fb8dea5fb
ChengyuanSha <15cs69@queensu.ca>
2021-10-28 10:38:17 -0400

    code optimization

--- src/general_GNN.py
+++ src/general_GNN.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 import torch_geometric.nn as pyg_nn
 
 
-
 class GNNStack(nn.Module):
     ''' The GNN Model '''
+
     def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim):
         super(GNNStack, self).__init__()
         # graph convolution layers
@@ -27,14 +27,13 @@ class GNNStack(nn.Module):
         self.dropout = 0.25
         self.num_layers = 3
 
-
     def forward(self, x, edge_index):
         for i in range(self.num_layers):
             x = self.convs[i](x, edge_index)
             emb = x
             x = F.relu(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
-            if not i == self.num_layers - 1: # except last layer
+            if not i == self.num_layers - 1:  # except last layer
                 x = self.lns[i](x)
 
         x = self.post_mp(x)
@@ -43,4 +42,3 @@ class GNNStack(nn.Module):
 
     def loss(self, pred, label):
         return F.nll_loss(pred, label)
-
