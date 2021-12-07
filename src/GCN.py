d848609964edd1c3f53b50f7360a4d1a9f2c90b6
ChengyuanSha <15cs69@queensu.ca>
2021-12-07 17:50:19 -0500

    update documentation

--- src/GCN.py
+++ src/GCN.py
@@ -6,9 +6,10 @@ import torch_geometric.nn as pyg_nn
 
 
 class GCNStack(nn.Module):
-    ''' The GCN Model '''
+    """ The graph convolution network (GCN) model """
 
     def __init__(self, input_dim, hidden_dim1, hidden_dim2, hidden_dim3, output_dim, dropout=0.5):
+        """ model structure definition """
         super(GCNStack, self).__init__()
         # graph convolution layers
         self.convs = nn.ModuleList()
@@ -19,7 +20,7 @@ class GCNStack(nn.Module):
         self.lns = nn.ModuleList()
         self.lns.append(nn.LayerNorm(hidden_dim1))
         self.lns.append(nn.LayerNorm(hidden_dim2))
-        # post-message-passing
+        # post-message-passing multilayer perceptron
         self.post_mp = nn.Sequential(
             nn.Linear(hidden_dim3, hidden_dim3),
             nn.Linear(hidden_dim3, output_dim))
@@ -28,21 +29,23 @@ class GCNStack(nn.Module):
         self.num_layers = 3
 
     def forward(self, x, edge_index):
+        """ define how my model is going to be run, from input to output """
         for i in range(self.num_layers):
             x = self.convs[i](x, edge_index)
-            emb = x
+            emb = x # save embeddings for visualization
             x = F.relu(x)
-            # add dropout layer
+            # add dropout layer for regularization
             x = F.dropout(x, p=self.dropout, training=self.training)
             if not i == self.num_layers - 1:  # except last layer
                 x = self.lns[i](x)
-
+        # post-message-passing
         x = self.post_mp(x)
 
         return emb, F.log_softmax(x, dim=1)
 
     def loss(self, pred, label):
-        ''' Define loss functions in NN, L2 norm regularization is implemented in the SGD optimizer weight decay  '''
-        # return  F.nll_loss(pred, label) # nn.CrossEntropyLoss()(pred, label)
+        """
+        Define cross entropy loss in GCN,
+        L2 norm regularization is implemented as weight decay
+        """
         return nn.CrossEntropyLoss()(pred, label)
-
