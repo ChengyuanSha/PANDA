36451202c3941569580db77f29094f609db59b45
ChengyuanSha <15cs69@queensu.ca>
2021-12-07 14:59:15 -0500

    update results

--- src/read_data.py
+++ src/read_data.py
@@ -11,6 +11,7 @@ import os
 
 script_dir = os.path.dirname(__file__)  # absolute dir the script is in
 
+
 def read():
     """ read the input data into the pyG Data class format """
     edge_list_path = os.path.join(script_dir, "../data/HMIN_edgelist.csv")
@@ -24,18 +25,21 @@ def read():
     autism_df = autism_df.drop_duplicates(subset='entrez_id', keep="last")
     autism_df = autism_df.reset_index()
 
-    # get the node features
-    # G = G.subgraph(autism_nodes)
+    # get graph edge index
     edge_index = get_edge_index(G)
 
+    # get the node features
     x = get_node_features(G)
 
-    train_mask, test_mask, val_mask, y = get_train_test_label(G, autism_df)
+    # train_mask, test_mask, val_mask, and y label for nodes
+    train_mask, test_mask, val_mask, y = get_masks_ylabel(G, autism_df)
 
+    # number of classes in the dataset
     num_classes = len(np.unique(y))
 
+    # to pyG Data class format
     data = Data(edge_index=edge_index, x=x, y=y, train_mask=train_mask, test_mask=test_mask,
-                val_mask = val_mask, num_classes=num_classes)  # .t().contiguous()
+                val_mask=val_mask, num_classes=num_classes)
 
     return data
 
@@ -43,6 +47,7 @@ def read():
 def get_node_features(G):
     """ use hand designed node features or not """
     x = torch.eye(G.number_of_nodes(), dtype=torch.float)
+    # Below are hand-engineered features, but not required in GNN
     # # feature: node degree
     # degrees = torch.tensor([val for (node, val) in G.degree()], dtype=torch.float)
     # # closeness
@@ -53,7 +58,6 @@ def get_node_features(G):
     # ec = torch.tensor([val for (node, val) in nx.eigenvector_centrality(G).items()], dtype=torch.float)
     # # feature: page rank
     # pr = torch.tensor([val for (node, val) in nx.pagerank(G, alpha=0.9).items()], dtype=torch.float)
-    #
     # x = torch.stack((degrees, closeness, betweenness, pr, ec)).t()
     return x
 
@@ -67,14 +71,14 @@ def get_edge_index(G):
     return edge_index
 
 
-def get_train_test_label(G, autism_df, sample_balanced_class=True, assign_unlabeled_using_community=False):
+def get_masks_ylabel(G, autism_df, sample_balanced_class=False, assign_unlabeled_using_community=True):
     """ Get the training, testing mask, and y labels """
     # get the labeled autism nodes position in the node list
     autism_nodes = autism_df['entrez_id'].to_numpy()
     all_nodes = np.array(G.nodes())
 
     # Find the labeled index in all_nodes, all_nodes are unique
-    labeled_index = [np.where(all_nodes == i)[0][0] for i in autism_nodes]
+    labeled_index = np.array([np.where(all_nodes == i)[0][0] for i in autism_nodes])
     label_mask = np.zeros(all_nodes.shape[0], dtype=bool)
     label_mask[labeled_index] = True
 
@@ -84,46 +88,55 @@ def get_train_test_label(G, autism_df, sample_balanced_class=True, assign_unlabe
 
     # initialize y_label, train_mask, test_mask to whole graph size
     y_label = np.zeros(all_nodes.shape[0], dtype=np.compat.long)
-    y_label[:] = 2  # temporarily set class 4 to unlabeled data
+    y_label[:] = 2  # set a class to unlabeled data
+    # set labeled node in graph
+    y_label[labeled_index] = autism_df['label'].to_numpy()
+
+    # initialize masks
     train_mask = np.zeros(all_nodes.shape[0], dtype=bool)
     test_mask = np.zeros(all_nodes.shape[0], dtype=bool)
     val_mask = np.zeros(all_nodes.shape[0], dtype=bool)
-
-    # set labeled node in graph
-    y_label[labeled_index] = autism_df['label'].to_numpy()
 
     # use communities info to assign a label to unlabelled data
     if assign_unlabeled_using_community:
         partion = community_louvain.best_partition(G)
         for c, i in enumerate(all_nodes):
             if c not in labeled_index:
-                y_label[c] = partion[i] + 2 # skip first 2 classes
+                y_label[c] = partion[i] + 2  # skip first 2 classes
 
     # randomly assign train test masks
-    y_length = len(autism_df.index)
+    y_length = len(autism_df.index) # how many labeled nodes
     y_index = np.arange(y_length)
 
     if sample_balanced_class:
         # random sample 150 samples for each class
-        train_index = np.array([np.random.choice(autism_df.index[autism_df['label']==i].tolist(), 150, replace=False) for i in range(2)]).flatten()
-        remaining_index = np.array([i for i in y_index if i not in train_index])
-        validation_index = remaining_index[:800]
-        test_index = remaining_index[800:]
+        train_index = np.array([np.random.choice(autism_df.index[autism_df['label'] == i].tolist(), 180, replace=False)
+                                for i in range(autism_df['label'].nunique())]).flatten()
+        remaining_index = np.random.permutation(np.array([i for i in y_index if i not in train_index]))
+        validation_index = remaining_index[:780]
+        test_index = remaining_index[780:]
+        # compute the actual index in mask array
+        train_index = labeled_index[train_index]
+        validation_index = labeled_index[validation_index]
+        test_index = labeled_index[test_index]
     else:
-        # 75% train, 25% test, not using this split method in this study
+        # 70% train, 15% validation, 15% testing
         max_train = int(y_length * 0.7)
         permutated_list = np.random.permutation(y_index)
         train_index = permutated_list[:max_train]
-        test_index = validation_index = permutated_list[max_train:]
-        # remaining_index = permutated_list[max_train:]
-        # validation_index = remaining_index[:int(len(remaining_index)*0.3)]
-        # test_index = remaining_index[int(len(remaining_index)*0.3):]
+        # test_index = validation_index = permutated_list[max_train:]
+        remaining_index = permutated_list[max_train:]
+        validation_index = remaining_index[:int(len(remaining_index) * 0.5)]
+        test_index = remaining_index[int(len(remaining_index) * 0.5):]
+        # compute the actual index in mask array
+        train_index = labeled_index[train_index]
+        validation_index = labeled_index[validation_index]
+        test_index = labeled_index[test_index]
 
-        # Using multiple levels of boolean index mask to assign train and test masks
-    # 1. index to labelled indexes 2. assign to train/test mask to True
-    train_mask.flat[np.flatnonzero(label_mask)[train_index]] = True
-    test_mask.flat[np.flatnonzero(label_mask)[test_index]] = True
-    val_mask[np.flatnonzero(label_mask)[validation_index]] = True
+    # update train, test, val mask
+    train_mask[train_index] = True
+    test_mask[test_index] = True
+    val_mask[validation_index] = True
 
     return torch.tensor(train_mask, dtype=torch.bool), torch.tensor(test_mask, dtype=torch.bool), \
            torch.tensor(val_mask, dtype=torch.bool), torch.tensor(y_label, dtype=torch.long)
